b5416ea16441a4aed5eb790ee35927e2e4d4c2d9
EvanBenitez <70672495+EvanBenitez@users.noreply.github.com>
2020-09-03 13:21:07 -0400

    Move everything to one .c file for mipsParser
    Everything for the mips_par function including the test main function.  Think it will be easier this way.

--- mipsParser.c
+++ mipsParser.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "mips_par.h"
 
-
-
-//int mips_par(FILE *,struct mips_line *);
-
-
+//THis structure contains the lable, instruction/directive, and the opperands.
+//a non existant element is denoted by an empty string.
+struct mips_line {
+  char lable[40];     //mips line lable
+  char inst_dir[15];  //mips directive or instruction
+  char op1[40];      //first operand
+  char op2[40];      //second operand
+  char op3[40];      //third operand
+};
 
 //parse a single line of mips file, returns 1 if line read or 0 if end of file
+//loads one line of a mips file into the given mips_line.
+//returns 1 if a line is loaded and 0 if end of file.
 int mips_par(FILE *in,struct mips_line *line){
   int index = 0;
   char sample[100];
@@ -51,4 +56,27 @@ int mips_par(FILE *in,struct mips_line *line){
     sscanf(sample,"%s %s %s %s %s",line->lable, line->inst_dir, line->op1, line->op2, line->op3);
   }
   return 1;
+}
+
+//test for mips_par
+int main(int argc,char *argv[]){
+  if( argc == 2){
+    struct mips_line line;
+    FILE *mips_file;
+    if((mips_file = fopen(argv[1],"r")) == NULL){
+      perror("Unable to open file");
+      exit(0);
+    }
+
+    while(mips_par(mips_file, &line)){
+      printf("%s \n",line.lable);
+      printf("%s \n",line.inst_dir);
+      printf("%s \n",line.op1);
+      printf("%s \n",line.op2);
+      printf("%s \n",line.op3);
+    }
+  }
+  else{
+    printf("Improper input");
+  }
 }
